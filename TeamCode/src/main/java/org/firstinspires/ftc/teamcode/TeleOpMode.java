98920b9f3c6ef3d4745979718ff0f33f7c390299
CodeSplash347 <manchestercedrek@gmail.com>
2025-10-15 19:36:53 -0400

    Created a base TeleOp and Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Const;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@TeleOp
+public class TeleOpMode extends OpMode {
+    Follower follower;
+    double slowModeMult = 1;
+    boolean isRobotCentric = true;
+    @Override
+    public void init() {
+        follower  = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose());
+        follower.update();
+    }
+
+    @Override
+    public void start() {
+        follower.startTeleopDrive(true);
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+        if (gamepad1.rightBumperWasPressed()) {
+            slowModeMult = (slowModeMult == 0.5) ? 1 : 0.5;
+        }
+
+        if (gamepad1.leftBumperWasPressed()) {
+            isRobotCentric = !isRobotCentric;
+        }
+
+        follower.setTeleOpDrive(
+                gamepad1.left_stick_y * slowModeMult,
+                gamepad1.left_stick_x * slowModeMult,
+                gamepad1.right_stick_x * slowModeMult,
+                isRobotCentric
+        );
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -1,17 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
 @Autonomous(name = "Example Auto", group = "Examples")
 public class TestAuto extends OpMode {
+    Follower follower;
+    public PathChain Path1;
     @Override
     public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(20.206, 122.656, Math.toRadians(323)));
+        Path1 = follower.pathBuilder().addPath(
+                new BezierLine(new Pose(20.206, 122.656),
+                        new Pose(72.285, 72.000)))
+                .setLinearHeadingInterpolation(Math.toRadians(-37),
+                         Math.toRadians(180))
+                .build();
 
+    }
+
+    @Override
+    public void start() {
+        follower.followPath(Path1, true);
     }
 
     @Override
     public void loop() {
+        follower.update();
 
     }
+
+
 }
